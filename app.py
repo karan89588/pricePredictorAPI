9925093add7df864f3585de8eb64913aa7f40a4f
karan89588 <81355355+karan89588@users.noreply.github.com>
2023-08-23 11:10:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ import pythonScript
 uri='mongodb+srv://karanmouryadmp:karan@cluster0.fzwllfy.mongodb.net/?retryWrites=true&w=majority'
 client=MongoClient(uri,tls=True,tlsAllowInvalidCertificates=True)
 
-appp=Flask(__name__)
+app=Flask(__name__)
 CORS(app)
 
 mydatabase=client['flaskcrud']
 db=mydatabase['users']
 
 
-@appp.route('/',methods=['GET'])
+@app.route('/',methods=['GET'])
 def get():
     return jsonify({'msg':'Hello'})
 
-@appp.route('/users',methods=['GET'])
+@app.route('/users',methods=['GET'])
 def getUsers():
     users=[]
     for doc in db.find():
@@ -32,7 +32,7 @@ def getUsers():
         })
         return jsonify(users)
 
-@appp.route('/users',methods=['Put'])
+@app.route('/users',methods=['Put'])
 def update():
     data=pythonScript.predictPrice(request.json['name'],request.json['season'],request.json['state'],request.json['availability'],request.json['demand'])
     db.update_one({'_id':ObjectId(request.json['id'])},{'$set':{
